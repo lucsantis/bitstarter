0cffa5aff8ba35d97222233ef296e8665b3b83f8
Luciano Santis <lucsantisf@gmail.com>
2013-08-05 14:26:04 +0000

    web.js

--- web.js
+++ web.js
@@ -1,11 +1,18 @@
 var express = require('express');
-var read = require('fs');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) { 
   fs.readFileSync("index.html", "utf-8");
-  response.send('read');
+  response.send('buf');
+
+buf = new Buffer(256);
+len = buf.write('\u00bd + \u00bc = \u00be', 0);
+console.log(len + " bytes: " + buf.toString('utf', 0, len));
+
+
+
 });
 
 var port = process.env.PORT || 5000;
