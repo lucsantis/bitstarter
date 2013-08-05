1d84135ed6cab52c094f5e587f0e7e22ccf55e15
Luciano Santis <lucsantisf@gmail.com>
2013-08-05 20:20:12 +0000

    web.js

--- web.js
+++ web.js
@@ -2,11 +2,11 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 var buffer = fs.readFileSync('index.html');
-
+var text = buffer.toString('utf-8');
 
 app.get('/', function(request, response) { 
   
-  response.send('buffer.toString('utf-8')');
+  response.send(text);
 
 });
 
