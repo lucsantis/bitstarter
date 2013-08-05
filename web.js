0b654b5966df1efb1c6229a26bba1178b14b3894
Luciano Santis <lucsantisf@gmail.com>
2013-08-05 14:31:01 +0000

    web.js

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
   fs.readFileSync("index.html", "utf-8");
   response.send('buf');
 
-buf = new Buffer(256);
+var buf = new Buffer(256);
 len = buf.write('\u00bd + \u00bc = \u00be', 0);
 console.log(len + " bytes: " + buf.toString('utf', 0, len));
 
